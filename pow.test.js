22040c04203e00798648e936e9eb304938d8385a
Dylan Floyd <floyd800@gmail.com>
2021-10-29 10:41:15 -0700

    triangulate pow test to increase reliability

--- pow.test.js
+++ pow.test.js
@@ -3,8 +3,8 @@ const pow = require('./pow.js');
 describe('POW BANG BOOM', () => {
 
     test('pow correctly raises a base to the exponent power', () => {
-        const expected = 4;
-        const actual = pow(2, 2);
-        expect(actual).toEqual(expected);
-    })
+        expect(pow(2, 2)).toEqual(4);
+        expect(pow(4, 2)).toEqual(16);
+        expect(pow(2, 4)).toEqual(16);
+    });
 });
